c0056e11c4118fe209f5c58981db0100caedd1de
MataReDev <goudalmathys@gmail.com>
2024-07-09 15:47:19 +0200

    Update

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 const Header = () => {
     return (
         <header className="flex bg-yellow-800 p-4 text-center">
-            <Link to={`/`}> <img src="https://cdn-icons-png.flaticon.com/512/7133/7133312.png" className='h-12'/> </Link>
+            <Link to={`/`}> <img src="https://cdn-icons-png.flaticon.com/512/7133/7133312.png" className='h-12' alt="Website Logo"/> </Link>
             <h1 className="text-white text-2xl font-bold mx-auto">World Of Tanks : Stats</h1>
         </header>
     );
